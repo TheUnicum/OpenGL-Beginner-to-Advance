c08aa0dd55c28d124cf2caca5cc0bd2fae6c0972
Mattia <tia_ben@tin.it>
2018-11-22 01:11:44 +0100

    Vertex Arrays [ and enable #core profile 330].

--- OpenGL-Beginner-to-Advance/src/Application.cpp
+++ OpenGL-Beginner-to-Advance/src/Application.cpp
@@ -120,6 +120,11 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //GLFW_OPENGL_COMPAT_PROFILE
+	//glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
@@ -149,6 +154,10 @@ int main(void)
 		0, 1, 2,
 		2, 3, 0,
 	};
+
+	unsigned int vao;
+	GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao));
 
 	unsigned int buffer;
 	GLCall(glGenBuffers(1, &buffer));
@@ -180,7 +189,10 @@ int main(void)
 	}
 	GLCall(glUniform4f(location, 0.3f, 0.3f, 0.8f, 1.0f));
 
+	GLCall(glUseProgram(0));
+	GLCall(glBindVertexArray(0));
 	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
 
 	float r = 0.0f;
 	float increment = 0.05f;
@@ -191,7 +203,17 @@ int main(void)
 		/* Render here */
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+		GLCall(glUseProgram(shader));
 		GLCall(glUniform4f(location, r, r, r, 1.0f));
+
+		GLCall(glBindVertexArray(vao));
+		// BindVertexArray <=> Bind-Buffers & Enable-Layout
+		//GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+		//GLCall(glEnableVertexAttribArray(0));
+		//GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, (2 + 3) * sizeof(float), (void*)0));
+
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
 		if (r > 1.0f)
