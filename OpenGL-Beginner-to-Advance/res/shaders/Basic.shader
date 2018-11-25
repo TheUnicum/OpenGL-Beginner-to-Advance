f371d629e37ec6252bc15da900a7d31b082e00fe
Mattia <tia_ben@tin.it>
2018-11-25 02:45:37 +0100

    Abstracting code part07 <Texture> & <stb_image>.

--- OpenGL-Beginner-to-Advance/res/shaders/Basic.shader
+++ OpenGL-Beginner-to-Advance/res/shaders/Basic.shader
@@ -1,27 +1,33 @@
 #shader vertex
 #version 330 core
 
-layout(location = 0) in vec4 position;
-layout(location = 1) in vec4 color;
+layout(location = 0) in vec4 vs_position;
+layout(location = 1) in vec4 vs_color;
+layout(location = 2) in vec2 vs_texCoord;
 
 out vec4 v_color;
+out vec2 v_texCoord;
 
 void main()
 {
-	gl_Position = position;
-	v_color = color;
-}
-;
+	gl_Position = vs_position;
+	v_color = vs_color;
+	v_texCoord = vs_texCoord;
+};
 
 #shader fragment
 #version 330 core
 
+layout(location = 0) out vec4 fs_color;
+
 in vec4 v_color;
-out vec4 color;
+in vec2 v_texCoord;
 
 uniform vec4 u_Color;
+uniform sampler2D u_Texture;
 
 void main()
 {
-	color = v_color + u_Color;
+	vec4 texColor = texture(u_Texture, v_texCoord);
+	fs_color = texColor + vec4(u_Color.xyz, 0.0f);
 };
--- OpenGL-Beginner-to-Advance/src/Application.cpp
+++ OpenGL-Beginner-to-Advance/src/Application.cpp
@@ -9,6 +9,7 @@
 #include "VertexArray.h"
 #include "Shader.h"
 #include "Renderer.h"
+#include "Texture.h"
 
 int main(void)
 {
@@ -35,6 +36,8 @@ int main(void)
 	glfwMakeContextCurrent(window);
 
 	glfwSwapInterval(1);	// active or deactive syncronize frame rate to 60Hz
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	if (glewInit() != GLEW_OK)
 		std::cout << "Error!" << std::endl;
@@ -42,10 +45,10 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	float positions[] = {
-		-0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-		+0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-		+0.5f, +0.5f, 0.0f, 0.0f, 1.0f,
-		-0.5f, +0.5f, 1.0f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+		+0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+		+0.5f, +0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+		-0.5f, +0.5f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
 	};
 
 	unsigned int indices[] = {
@@ -56,11 +59,12 @@ int main(void)
 	{	// make a scope to force delete a VertexBuffer before context windows get distroyed
 		VertexArray va;
 
-		VertexBuffer vb(positions, 4 * (2 + 3) * sizeof(float));
+		VertexBuffer vb(positions, 4 * (2 + 3 + 2) * sizeof(float));
 
 		VertexBufferLayout layout;
 		layout.Push<float>(2);
 		layout.Push<float>(3);
+		layout.Push<float>(2);
 		va.AddBuffer(vb, layout);
 
 		IndexBuffer ib(indices, 6);
@@ -68,6 +72,10 @@ int main(void)
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.3f, 0.3f, 0.8f, 1.0f);
+
+		Texture texture("res/textures/ChernoLogo.png");
+		texture.Bind(0);
+		shader.SetUniform1i("u_Texture", 0);
 
 		shader.Unbind();
 		va.Unbind();
